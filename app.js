9bd8c35eeaea5d4040597be6b48adb76e6a32eeb
Mostak Ahmed <mosak.ahm263@gmail.com>
2024-01-16 09:40:17 +0600

    create main code

---
+++ app.js
@@ -0,0 +1,7 @@
+const {obj, func} = require('./utils');
+
+console.log(obj)
+
+const value = func(2, 3);
+
+console.log(value)
---
+++ utils.js
@@ -0,0 +1,11 @@
+const obj = {
+    name: 'Mostak Ahmed',
+    age: '23'
+}
+
+const func = (a, b) => a + b;
+
+module.exports = {
+    obj,
+    func
+}
